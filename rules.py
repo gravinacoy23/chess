457f1ef05ad999d2d049c69c4aadc2eef0252edf
gravinacoy23 <juangravinacoy@gmail.com>
2024-04-21 19:24:52 -0500

    Refactor castle()
    Refactor the function to castle so that is easier to work with and more
    redeable

--- rules.py
+++ rules.py
@@ -281,26 +281,44 @@ class Rules:
             piece_slice (slice): Slice object to access the name of the piece
             turn (str): String witht the current turn Black or Whtie
         """
-        if selected_piece_row == selected_square_row and selected_piece[color_slice] == turn:
+        if selected_piece_col < selected_square_col:
+            squares_move = 2
+            rook_pos = 3
+            rook_direction = -1
+            king_direction = 2
+            iter_direction = 1
+        else:
+            squares_move = 3
+            rook_pos = -4
+            rook_direction = 1
+            king_direction = -2
+            iter_direction = 1
+
+        if selected_piece[color_slice] == "White":
+            starting_row = 7
+        else:
+            starting_row = 0
+
+        if selected_piece_row == starting_row:
             if (
-                selected_piece_col + 2 == selected_square_col
-                and board[selected_piece_row][selected_piece_col + 3][piece_slice]
+                selected_piece_col + king_direction == selected_square_col
+                and board[selected_piece_row][selected_piece_col + rook_pos][
+                    piece_slice
+                ]
                 == "Rook"
             ):
-                self.valid_moves.append((selected_square_row, selected_square_col))
-                board[selected_piece_row][selected_piece_col + 3] = None
+                for i in range(1, squares_move + 1):
+                    if (
+                        board[selected_piece_row][
+                            selected_piece_col + i * iter_direction
+                        ]
+                        != None
+                    ):
+                        return
+                self.valid_moves.append((selected_piece_row, selected_square_col))
+                board[selected_piece_row][selected_piece_col + rook_pos] = None
                 board[selected_piece_row][
-                    selected_square_col - 1
-                ] = f"{selected_piece[color_slice]} Rook"
-            elif (
-                selected_piece_col - 2 == selected_square_col
-                and board[selected_piece_row][selected_piece_col + 3][piece_slice]
-                == "Rook"
-            ):
-                self.valid_moves.append((selected_square_row, selected_square_col))
-                board[selected_piece_row][selected_piece_col - 4] = None
-                board[selected_piece_row][
-                    selected_square_col + 1
+                    selected_square_col + rook_direction
                 ] = f"{selected_piece[color_slice]} Rook"
 
     def define_valid_moves(
@@ -313,7 +331,7 @@ class Rules:
         piece_slice: slice,
         possible_passant: bool,
         possible_passant_col: int,
-        turn: str
+        turn: str,
     ):
         """This is the function that will call the functions that contain the logic to move each piece.
 
